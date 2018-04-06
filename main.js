901843175736705b5e791365ca6c7b4291278198
April Nichols <nicholsar@gmail.com>
2018-04-05 20:50:50 -0500

    build dom string function setup

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 const printToDom = (domString, divId) => {
     document.getElementById(divId).innerHTML = domString;
 }
+
+const buildDomString = (players) => {
+    let domString = '';
+    players.forEach((player) => {
+        domString += `<div class="player-card">`;
+        domString +=    `<h2>${player.name}</h2>`
+        domString +=    `<img src="${player.gravatar_url}">`;
+        domString +=    `<h3>${player.points.total}</h3>`;
+        domString += `</div>`;
+    });
+    printToDom(domString, "player-holder");
+}
