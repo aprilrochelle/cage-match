cfaf35f954cd8d2e47571c3357320b2f4ff1ff7f
April Nichols <nicholsar@gmail.com>
2018-04-06 19:34:57 -0500

    wip

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let player1Name = '';
+let player2Name = '';
+
 const printToDom = (domString, divId) => {
     document.getElementById(divId).innerHTML = domString;
 }
@@ -16,22 +19,35 @@ const buildDomString = (players) => {
 
 const attachEventListener = () => {
     const matchBtn = document.getElementById('match-btn');
-    matchBtn.addEventListener('click', parsePlayerData);
+    matchBtn.addEventListener('click', grabPlayerNames);
 }
 
-const parsePlayerData = () => {
-    
+const grabPlayerNames = () => {
+    player1Name = document.getElementById('player1').value;
+    player2Name = document.getElementById('player2').value;
+    grabPlayerJson(player1Name, player2Name);
+}
+
+const grabPlayerJson = (player1, player2) => {
+    const player1Data = `"https://teamtreehouse.com/${player1}.json"`;
+    const player2Data = `"https://teamtreehouse.com/${player2}.json"`;
+    playerXhrCall(player1Data);
+}
+
+function executeOnLoad() {
+    const data1 = JSON.parse(this.responseText);
+    console.log(data1);
 }
 
 function executeIfFail() {
     console.log('Something went wrong.');
 }
 
-const xhrCall = (userName, successFunction) => {
+const playerXhrCall = (playerData) => {
     let myRequest = new XMLHttpRequest();
-    myRequest.addEventListener('load', successFunction);
+    myRequest.addEventListener('load', executeOnLoad);
     myRequest.addEventListener('error', executeIfFail);
-    myRequest.open("GET", `https://teamtreehouse.com/${username}.json`);
+    myRequest.open("GET", playerData);
     myRequest.send();
 }
 
